797594275c42189b6a7f80754ab5727b02b7cf04
franciscopmbraz <ricardo.braz918@gmail.com>
2025-12-07 20:47:59 +0000

    nova45

--- src/main/java/jogo/interaction/WorldInteraction.java
+++ src/main/java/jogo/interaction/WorldInteraction.java
@@ -1,31 +1,52 @@
 package jogo.interaction;
 
 import com.jme3.bullet.PhysicsSpace;
+import jogo.gameobject.item.Inventory;
+import jogo.gameobject.item.ItemStack;
 import jogo.voxel.VoxelPalette;
 import jogo.voxel.VoxelWorld;
 
 public class WorldInteraction {
 
-
     public static void interactWithBlock(VoxelWorld vw, VoxelWorld.Vector3i cell, PhysicsSpace physicsSpace) {
 
-        // Descobrir qual é o bloco
+        // Descobrir qual é o bloco alvo
         byte idAtual = vw.getBlock(cell.x, cell.y, cell.z);
 
-        // Regra 1: Transformar Relva em Speed
+        // Obter o inventário e o item que está "na mão" (selecionado)
+        Inventory inv = Inventory.getGlobalInventory(); //
+        ItemStack stack = inv.getSelectedStack();       //
+
+        // Regra 1: Transformar Relva em Lavoura (Funciona sem item específico por enquanto)
         if (idAtual == VoxelPalette.GRASS_ID || idAtual == VoxelPalette.DIRT_ID) {
             vw.setBlock(cell.x, cell.y, cell.z, VoxelPalette.TILLED_ID);
 
-            // Só reconstruímos se houver mudanças físicas/visuais
             if (physicsSpace != null) {
                 vw.rebuildDirtyChunks(physicsSpace);
             }
         }
+
+        // Regra 2: Plantar Cenoura na Lavoura
         if (idAtual == VoxelPalette.TILLED_ID) {
-            vw.setBlock(cell.x, cell.y, cell.z, VoxelPalette.CARROT_ID);
-            // Só reconstruímos se houver mudanças físicas/visuais
-            if (physicsSpace != null) {
-                vw.rebuildDirtyChunks(physicsSpace);
+
+            // Verifica se existe um stack selecionado e se não está vazio
+            if (stack != null && !stack.isEmpty()) {
+
+                // Verifica se o item é a cenoura
+                // O CenouraItem define o nome como "cenoura" no construtor
+                if (stack.getItem().getName().equals("cenoura")) {
+
+                    // Planta a cenoura
+                    vw.setBlock(cell.x, cell.y, cell.z, VoxelPalette.CARROT_ID);
+
+                    // Consome 1 item do stack da mão
+                    stack.remove(1); //
+
+                    // Atualiza o mundo
+                    if (physicsSpace != null) {
+                        vw.rebuildDirtyChunks(physicsSpace);
+                    }
+                }
             }
         }
     }
